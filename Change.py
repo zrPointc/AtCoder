bbee8a0784b8c69487f07533f20e8e5ab64723f2
zrPointc <4uth9gotegote@gmail.com>
2026-09-17 20:21:29 +0900

    入力処理を修正し、コイン計算ロジックを改善

--- Change.py
+++ Change.py
@@ -1,16 +1,15 @@
 n = int(input())
-A = list(map(int, input().split()))
+A = list(map(str, input().split()))
 coin1, coin10, coin100 = 0, 0, 0
-
+calc = 0
 for i in range(n):
-    if (10 - A[i] % 10) % 10 == 0:
-        coin1 += 10 - A[i] % 10
-    if 10 - A[i] // 10 % 10 == 0:
-        coin10 += 10 - A[i] // 10 % 10
+    if A[i] != "1":
+        calc = 1000 * (int(A[i][0]) + 1) - int(A[i])
+    else:
+        calc = 999
 
-    while A[i] > 1000:
-        A[i] -= 1000
-
-    coin100 += 10 - A[i] // 100 % 10
+    coin1 += calc % 10
+    coin10 += calc % 100 // 10
+    coin100 += (calc % 1000) // 100
 
 print(coin1, coin10, coin100)
